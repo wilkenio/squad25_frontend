89b767c68636b75ffcaae0705dede8c5f7ad84b8
wilkenio <wilkeniopereiraguitarra@gmail.com>
2025-03-29 14:29:59 -0300

    📦 NEW: implementadno componente principais despesas

---
+++ src/app/components/dashboard/pricipais-despesas/pricipais-despesas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricipais-despesas',
+  imports: [],
+  templateUrl: './pricipais-despesas.component.html',
+  styleUrl: './pricipais-despesas.component.css'
+})
+export class PricipaisDespesasComponent {
+
+}
