24f41b7ae771e02d6675c2fa6a0a4d86e8f3d232
wilkenio <wilkeniopereiraguitarra@gmail.com>
2025-03-31 08:23:51 -0300

    🐛 FIX: ajuste do menu para receber as opções e ajuste no layout mobile

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NovaContaComponent } from '../pop-up/nova-conta/nova-conta.component';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css'],
+  standalone: true,
+  imports: [NovaContaComponent],
 })
 export class MenuComponent {
+  @ViewChild(NovaContaComponent) novaContaComponent!: NovaContaComponent;
+  optionsVisivel = false; // Estado do menu de opções
 
+  toggleOptions() {
+    this.optionsVisivel = !this.optionsVisivel;
+  }
+
+  selecionarOpcao(acao?: Function) {
+    this.optionsVisivel = false; // Sempre esconde as opções
+    if (acao) {
+      acao(); // Executa a ação, se existir
+    }
+  }
+  
+  // Método para alternar a exibição do popup
+  toggleNovaContaPopup() {
+    this.novaContaComponent.togglePopup(); // Chama o método no NovaContaComponent
+  }
 }
