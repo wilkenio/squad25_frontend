560aab2a6651bef15bb5a4f2367cab2c3cd41c08
yuri <yurigx12345@gmail.com>
2025-03-31 17:23:19 -0300

    📦 NEW: Implementação do componente despesaspor-categoria

---
+++ src/app/components/dashboard/despesaspor-categoria/despesaspor-categoria.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-despesaspor-categoria',
+  imports: [],
+  templateUrl: './despesaspor-categoria.component.html',
+  styleUrl: './despesaspor-categoria.component.css'
+})
+export class DespesasporCategoriaComponent {
+
+}
