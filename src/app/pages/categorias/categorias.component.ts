fa233d49b33b6a9eeca518bed8eb9687b1edce6b
yuri <yurigx12345@gmail.com>
2025-04-08 18:37:18 -0300

    🐛 FIX: Adição de receitas, despesas e contas na pagina categorias

--- src/app/pages/categorias/categorias.component.ts
+++ src/app/pages/categorias/categorias.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sideBar/sideBar.component';
 import { MenuComponent } from '../../components/menu/menu.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-categorias',
-  imports: [SidebarComponent, MenuComponent],
+  standalone: true,
+  imports: [CommonModule, SidebarComponent, MenuComponent],
   templateUrl: './categorias.component.html',
-  styleUrl: './categorias.component.css'
+  styleUrls: ['./categorias.component.css']
 })
 export class CategoriasComponent {
+  abaSelecionada: 'receitas' | 'despesas' | 'contas' = 'receitas';
 
+  categoriasReceitas = [
+    { descricao: 'Salário', total: 0 },
+    { descricao: 'Trabalhos extras', total: 830 },
+    { descricao: 'Bonificações', total: 10 },
+    { descricao: 'Bonificações', total: 0 }
+  ];
+  
+
+  subcategoriasTrabalhoExtra = [
+    { descricao: 'Freela de software', total: 100 },
+    { descricao: 'Cachorro quente na praia', total: 150 },
+    { descricao: 'Lavar carros', total: 300 },
+  ];
+
+  calcularTotalReceitas(): number {
+    return this.categoriasReceitas.reduce((soma, cat) => soma + cat.total, 0);
+  }
+
+  calcularTotalSubcategorias(): number {
+    return this.subcategoriasTrabalhoExtra.reduce((soma, sub) => soma + sub.total, 0);
+  }
+  
+  toggleOptions() {
+    console.log('Ícone de adicionar clicado!');
+  }
+  
+  
 }
